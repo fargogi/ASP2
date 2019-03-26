174e023afa39d9119ad91955e43f226d3b5d9554
StepanyukAN <fargogi@mail.ru>
2019-03-26 19:34:51 +1000

    Ненавязчивый Ajax

---
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebStore.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> GetTestView()
+        {
+            await Task.Delay(2000);
+            return PartialView("Partial/_DataView", DateTime.Now);
+        }
+    }
+}
